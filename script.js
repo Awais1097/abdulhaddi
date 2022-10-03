070623057a1aa26de113a6323b1a4e63def6c08a
Awais ul Hassan <73490602+Awais1097@users.noreply.github.com>
2022-10-03 01:03:41 -0700

    Add files via upload
    Signed-off-by: Awais ul Hassan <73490602+Awais1097@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Generate random room name if needed
 if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
 const roomName = 'observable-' + roomHash;
@@ -53,29 +53,29 @@ function connection() {
             members.splice(index, 1);
             updateMembersDOM();
         });
-//     room.on('data', (message, client) => {
-//      console.log('message', message);
-//      console.log('client', client );
-// 	 if (message.text) {
-// 		if (client) {
-//  		msgs.push(message);
-//                 addMessageToListDOM(message, client);
-// 		}
-//             } else {
-//                 // Message is from server
-//             }
-    
-//     });
+             room.on('data', (message, client) => {
+             console.log('message', message);
+              console.log('client', client );
+         	 if (message.text) {
+         		if (client) {
+          		msgs.push(message);
+                         addMessageToListDOM(message, client);
+        		}
+                     } else {
+                         // Message is from server
+                     }
 
-      //  room.on('data', (text, member) => {
-         //   messages.push(text.toString() + member.toString());
-         //   console.log(text, member);
-          //  if (member) {
-          //      addMessageToListDOM(text, member);
-           // } else {
-                // Message is from server
-         //   }
-       //});
+             });
+
+        //  room.on('data', (text, member) => {
+        //   messages.push(text.toString() + member.toString());
+        //   console.log(text, member);
+        //  if (member) {
+        //      addMessageToListDOM(text, member);
+        // } else {
+        // Message is from server
+        //   }
+        //});
 
     });
 
@@ -93,7 +93,7 @@ function updateMembersDOM() {
     members.forEach(member =>
         DOM.membersList.appendChild(createMemberElement(member))
     );
-   // DOM.membersList.appendChild(createMemberElementCount(members.length + ''));
+    // DOM.membersList.appendChild(createMemberElementCount(members.length + ''));
 }
 
 
@@ -102,12 +102,12 @@ function createMemberElement(member) {
     const el = document.createElement('a');
     el.textContent = name;//appendChild(document.createTextNode(name));
     el.className = 'participant-more';
-   // el.style.color = color;
-        if (member.id === drone.clientId) {
-            document.getElementById("local-name").textContent = name;
-        }else{
-            document.getElementById("remote-name").textContent = name;
-        }
+    // el.style.color = color;
+    if (member.id === drone.clientId) {
+        document.getElementById("local-name").textContent = name;
+    } else {
+        document.getElementById("remote-name").textContent = name;
+    }
     return el;
 }
 
@@ -122,10 +122,10 @@ function createMemberElementCount(member) {
 function addMessageToListDOM(text, client) {
     const el = DOM.messages;
     el.appendChild(createMessageElement(text, client));
-     const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
-   // if (wasTop) {
-        el.scrollTop = el.scrollHeight;
-   // }
+    const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+    // if (wasTop) {
+    el.scrollTop = el.scrollHeight;
+    // }
 }
 
 function createMessageElement(text, client) {
@@ -140,110 +140,110 @@ function createMessageElement(text, client) {
     eld.appendChild(elc);
     eld.appendChild(elm);
     eld.className = 'message-content';
-  if (client.id === drone.clientId) {
-    const el = document.createElement('div');
-    el.appendChild(eld);
-    el.className = 'message-wrapper reverse';
-    return el;	
-  }else{
-    const el = document.createElement('div');
-    el.appendChild(eld);
-    el.className = 'message-wrapper';
-    return el;
-  }
-    
+    if (client.id === drone.clientId) {
+        const el = document.createElement('div');
+        el.appendChild(eld);
+        el.className = 'message-wrapper reverse';
+        return el;
+    } else {
+        const el = document.createElement('div');
+        el.appendChild(eld);
+        el.className = 'message-wrapper';
+        return el;
+    }
+
 }
 
 // Room name needs to be prefixed with 'observable-'
 const configuration = {
-  iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
-  }]
+    iceServers: [{
+        urls: 'stun:stun.l.google.com:19302'
+    }]
 };
 
-  
-  
-function onSuccess() {};
+
+
+function onSuccess() { };
 function onError(error) {
-  console.error(error);
+    console.error(error);
 };
- 
-  
+
+
 // Send signaling data via Scaledrone
 function sendMessage(message) {
     console.log('message', message);
 
-  drone.publish({
-    room: roomName,
-    message
-  });
- document.getElementById("chat-input").value = '';
+    drone.publish({
+        room: roomName,
+        message
+    });
+    document.getElementById("chat-input").value = '';
 }
 //DOM.form.addEventListener('submit', sendMessage());
 
 function startWebRTC(isOfferer) {
-  pc = new RTCPeerConnection(configuration);
-  
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
-  pc.onicecandidate = event => {
-    if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
-    }
-  };
-  
-  // If user is offerer let the 'negotiationneeded' event create the offer
-  if (isOfferer) {
-    pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
-    }
-  }
-  
-  // When a remote stream arrives display it in the #remoteVideo element
-  pc.onaddstream = event => {
-    remoteVideo.srcObject = event.stream;
-  };
-  
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  }).then(stream => {
-    // Display your local video in #localVideo element
-    localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
-    pc.addStream(stream);
-  }, onError);
-  
-  // Listen to signaling data from Scaledrone
-  room.on('data', (message, client) => {
-    // Message was sent by us
-    if (client.id === drone.clientId) {
-      return;
-    }
-  
-    if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
-        if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
+    pc = new RTCPeerConnection(configuration);
+
+    // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
+    // message to the other peer through the signaling server
+    pc.onicecandidate = event => {
+        if (event.candidate) {
+            sendMessage({ 'candidate': event.candidate });
         }
-      }, onError);
-    } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
-      );
-    } 
-  });
+    };
+
+    // If user is offerer let the 'negotiationneeded' event create the offer
+    if (isOfferer) {
+        pc.onnegotiationneeded = () => {
+            pc.createOffer().then(localDescCreated).catch(onError);
+        }
+    }
+
+    // When a remote stream arrives display it in the #remoteVideo element
+    pc.onaddstream = event => {
+        remoteVideo.srcObject = event.stream;
+    };
+
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+    }).then(stream => {
+        // Display your local video in #localVideo element
+        localVideo.srcObject = stream;
+        // Add your stream to be sent to the conneting peer
+        pc.addStream(stream);
+    }, onError);
+
+    // Listen to signaling data from Scaledrone
+    room.on('data', (message, client) => {
+        // Message was sent by us
+        if (client.id === drone.clientId) {
+            return;
+        }
+
+        if (message.sdp) {
+            // This is called after receiving an offer or answer from another peer
+            pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+                // When receiving an offer lets answer it
+                if (pc.remoteDescription.type === 'offer') {
+                    pc.createAnswer().then(localDescCreated).catch(onError);
+                }
+            }, onError);
+        } else if (message.candidate) {
+            // Add the new ICE candidate to our connections remote description
+            pc.addIceCandidate(
+                new RTCIceCandidate(message.candidate), onSuccess, onError
+            );
+        }
+    });
 }
-  
+
 function localDescCreated(desc) {
-  pc.setLocalDescription(
-    desc,
-    () => sendMessage({'sdp': pc.localDescription}),
-    onError
-  );
+    pc.setLocalDescription(
+        desc,
+        () => sendMessage({ 'sdp': pc.localDescription }),
+        onError
+    );
 }
 
 const switchMode = document.querySelector('button.mode-switch'),
@@ -251,7 +251,7 @@ const switchMode = document.querySelector('button.mode-switch'),
     closeBtn = document.querySelector('.btn-close-right'),
     rightSide = document.querySelector('.right-side'),
     expandBtn = document.querySelector('.expand-btn');
-    videor = document.querySelector('.btn-muter');
+videor = document.querySelector('.btn-muter');
 
 
 switchMode.addEventListener('click', () => {
@@ -271,7 +271,7 @@ function getURL() {
 }
 
 function getMessage() {
-    return {'text': DOM.input.value, 'name':  document.getElementById("local-name").textContent+'.\t'};
+    return { 'text': DOM.input.value, 'name': document.getElementById("local-name").textContent + '.\t' };
 }
 
 function copy(input) {
@@ -279,7 +279,7 @@ function copy(input) {
     if (navigator.clipboard) {
         navigator.clipboard.writeText(input).then(() => {
             console.log('Copied to clipboard successfully.');
-            window.alert('Link: '+input+'\nCopied to clipboard successfully.')
+            window.alert('Link: ' + input + '\nCopied to clipboard successfully.')
         }, (err) => {
             console.log('Failed to copy the text to clipboard.', err);
         });
