d243a538bd0ccce52b53393ad8b987a563e85953
Awais ul Hassan <73490602+Awais1097@users.noreply.github.com>
2023-08-13 08:41:30 -0700

    Update script.js
    Signed-off-by: Awais ul Hassan <73490602+Awais1097@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Generate random room name if needed
-if (!location.hash) {
-    location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
+//if (!location.hash) {
+ //   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+//}
 
 
 
